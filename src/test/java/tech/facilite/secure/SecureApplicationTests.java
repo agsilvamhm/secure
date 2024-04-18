164f676e261c5fe2e56bd84f50cf9664b71ecc43
agsilvamhm <adalberto.goncalves.silva@gmail.com>
2024-04-18 20:34:40 -0300

    First commit

---
+++ src/test/java/tech/facilite/secure/SecureApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.facilite.secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
